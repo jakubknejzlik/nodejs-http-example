542e325ea82105345ef66143ecb8d1e9b2127419
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2015-03-09 01:26:45 +0100

    app commit

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(10001, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:10001/');
+}).listen(10003, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:10003/');
